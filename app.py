d710328d531c260c908c5023075e308039f7b19a
Gustavo <gustavobiggg64@gmail.com>
2026-03-03 23:58:23 -0300

    feat: created register

--- app.py
+++ app.py
@@ -1,19 +1,34 @@
 from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-user_ = "user"
-password_ = "password"
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'login_system'
+app.config['MYSQL_PORT'] = 3306
 
+mysql = MySQL(app)
 
 @app.route("/", methods=["GET", "POST"])
 def login():
 
     if request.method == "POST":
         user = request.form["user"]
-        senha_digitada = request.form["password"]
+        password = request.form["password"]
 
-        if user == user_ and senha_digitada == password_:
+        cursor = mysql.connection.cursor()
+
+        cursor.execute(
+            "SELECT * FROM users WHERE user=%s AND password=%s",
+            (user, password)
+        )
+
+        result = cursor.fetchone()
+        cursor.close()
+
+        if result:
             return redirect("/next")
         else:
             return "Login inválido"
@@ -21,9 +36,32 @@ def login():
     return render_template("index.html")
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+        user = request.form["user"]
+        password = request.form["password"]
+
+        cursor = mysql.connection.cursor()
+
+        cursor.execute(
+            "INSERT INTO users (user, password) VALUES (%s,%s)",
+            (user, password)
+        )
+
+        mysql.connection.commit()
+        cursor.close()
+
+        return redirect("/")
+
+    return render_template("register.html")
+
+
 @app.route("/next")
 def next():
     return render_template("next.html")
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
